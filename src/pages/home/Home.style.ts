c1b7854beb8bd420114c3f263516e7bfa0f9283a
David <contato.davidalmeida@outlook.com>
2023-12-09 16:57:41 -0300

    ✅ Update: Home page

--- src/pages/home/Home.style.ts
+++ src/pages/home/Home.style.ts
@@ -1,3 +1,111 @@
 import styled from "styled-components";
 
-export const StyledHome = styled.div``;
+export const StyledHome = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 100vw;
+	height: 100vh;
+	overflow: hidden;
+
+	h2 {
+		color: white;
+		font-size: 48px;
+		font-family: var(--tipography-content--title);
+		font-weight: 400;
+	}
+
+	p {
+		color: white;
+		font-family: var(--tipography-content--description);
+		line-height: 24px;
+		font-weight: 400;
+	}
+
+	@media (min-width: 1024px) {
+		flex-direction: row;
+	}
+
+	&::after {
+		content: "";
+		position: fixed;
+		top: 0;
+		left: 0;
+		min-height: 100vh;
+		min-width: 100vw;
+
+		background: rgb(0, 0, 0);
+		background: linear-gradient(
+			0deg,
+			rgba(0, 0, 0, 1) 10%,
+			rgba(0, 0, 0, 0) 75%
+		);
+
+		@media (min-width: 768px) {
+			background: rgb(0, 0, 0);
+			background: linear-gradient(
+				120deg,
+				rgba(0, 0, 0, 1) 10%,
+				rgba(0, 0, 0, 0) 75%
+			);
+		}
+
+		@media (min-width: 1024px) {
+			background: rgb(0, 0, 0);
+			background: linear-gradient(
+				120deg,
+				rgba(0, 0, 0, 1) 40%,
+				rgba(0, 0, 0, 0) 75%
+			);
+		}
+	}
+`;
+
+export const StyledMain = styled.main`
+	display: flex;
+	flex: 1;
+	padding: 1.5rem;
+`;
+
+export const StyledSection = styled.section`
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+	gap: 2rem;
+
+	max-width: 500px;
+
+	position: relative;
+	z-index: 1;
+
+	@media (min-width: 1024px) {
+		justify-content: center;
+	}
+`;
+
+export const StyledNav = styled.nav`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	gap: 1rem;
+	padding: 1.5rem;
+
+	position: relative;
+	z-index: 1;
+
+	img {
+		border-radius: 50%;
+		width: 100%;
+	}
+
+	@media (min-width: 768px) {
+		justify-content: space-around;
+		background: rgba(0, 0, 0, 0.5);
+	}
+
+	@media (min-width: 1024px) {
+		flex-direction: column;
+		background: rgba(0, 0, 0, 0.5);
+		justify-content: center;
+	}
+`;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,44 @@
-import { StyledHome } from "./Home.style";
+import { useState } from "react";
+
+import { Button } from "../../components/Button";
+import { characters } from "../../mock/characters";
+import { icons } from "../../mock/icons";
+
+import OnePieceLogo from "../../assets/one-piece-logo.webp";
+
+import { StyledHome, StyledMain, StyledNav, StyledSection } from "./Home.style";
 
 export const Home = () => {
-	return <StyledHome>Project One Piece</StyledHome>;
+	const [count, setCount] = useState(0);
+
+	return (
+		<StyledHome
+			style={{
+				backgroundImage: `url(${characters[count].background})`,
+				backgroundSize: "cover",
+				backgroundPosition: "center",
+				objectFit: "cover",
+			}}
+		>
+			<StyledMain>
+				<StyledSection>
+					<img src={OnePieceLogo} alt="one piece logo" width={200} />
+					<h2>{characters[count].name}</h2>
+					<p>{characters[count].description}</p>
+				</StyledSection>
+			</StyledMain>
+			<StyledNav>
+				{icons.map((icon, index) => (
+					<Button
+						key={index}
+						onClick={() => setCount(index)}
+						ariaLabel={`click to see details about ${icon.alt}`}
+						className={index === count ? "selected" : ""}
+					>
+						<img src={icon.src} alt={icon.alt} />
+					</Button>
+				))}
+			</StyledNav>
+		</StyledHome>
+	);
 };
