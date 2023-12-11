7802f108f9a259525fd41dddd76387773c7d2dd5
David <contato.davidalmeida@outlook.com>
2023-12-11 11:13:54 -0300

    🔥 Remove code or files

--- src/interfaces/Button.ts
+++
@@ -1,7 +0,0 @@
-export interface ButtonProps {
-	type?: "button" | "submit" | "reset" | undefined;
-	ariaLabel?: string;
-	children?: React.ReactNode;
-	className?: string;
-	onClick?: () => void;
-}
--- src/interfaces/Character.ts
+++
@@ -1,5 +0,0 @@
-export interface Character {
-	name: string;
-	description: string;
-	background: string;
-}
